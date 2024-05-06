a51ea3989d1d21806b92f6b0cf0d3d2f4c0e4a9e
Jovan02 <jovannikolic2801@gmail.com>
2024-05-06 13:26:54 +0200

    Add router

--- client/front/src/App.js
+++ client/front/src/App.js
@@ -1,5 +1,52 @@
+import React from "react";
+import {
+    createBrowserRouter,
+    RouterProvider,
+    Route,
+    Outlet,
+} from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import "./styles/App.css";
+import Welcome from "./pages/Welcome";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Welcome />,
+    },
+    {
+        path: "/",
+        element: <Navbar />,
+        children: [
+            {
+                path: "/home",
+                element: <Home />,
+            },
+            {
+                path: "/browse",
+                element: <h1>Browse</h1>,
+            },
+        ],
+    },
+    {
+        path: "/register",
+        element: <Register />,
+    },
+    {
+        path: "/login",
+        element: <Login />,
+    },
+]);
+
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div className="App">
+            <RouterProvider router={router} />
+        </div>
+    );
 }
 
 export default App;
