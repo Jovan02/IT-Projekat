8a96ee4c1f8aa97d82219c5f623f33399ab8aa54
Jovan02 <jovannikolic2801@gmail.com>
2024-05-07 19:33:49 +0200

    Add Profile design

---
+++ client/front/src/pages/profile.jsx
@@ -0,0 +1,105 @@
+import React from "react";
+import "../styles/Profile.css";
+const Profile = () => {
+    return (
+        <div class="main">
+            <div class="title">
+                <h1>CINEMAXX</h1>
+            </div>
+
+            <div class="profile-container">
+                <img
+                    class="img-profile"
+                    src="images/joker.jfif"
+                    alt="Profile Picture"
+                />
+
+                <p class="change-picture-text">Change profile picture</p>
+
+                <h2 class="profile-username">Username</h2>
+
+                <h2 class="profile-ticket-text">My tickets</h2>
+
+                <div class="ticket-container">
+                    <div class="ticket">
+                        <p>Movie name</p>
+                        <p>12.04.2024</p>
+                        <p>16:30</p>
+                    </div>
+                    <div class="ticket">
+                        <p>Movie name</p>
+                        <p>12.04.2024</p>
+                        <p>16:30</p>
+                    </div>
+                    <div class="ticket">
+                        <p>Aquaman and the lost kingdom</p>
+                        <p>12.04.2024</p>
+                        <p>16:30</p>
+                    </div>
+                    <div class="ticket">
+                        <p>Movie name</p>
+                        <p>12.04.2024</p>
+                        <p>16:30</p>
+                    </div>
+                    <div class="ticket">
+                        <p>Movie name</p>
+                        <p>12.04.2024</p>
+                        <p>16:30</p>
+                    </div>
+                    <div class="ticket">
+                        <p>Movie name</p>
+                        <p>12.04.2024</p>
+                        <p>16:30</p>
+                    </div>
+                    <div class="ticket">
+                        <p>Movie name</p>
+                        <p>12.04.2024</p>
+                        <p>16:30</p>
+                    </div>
+                    <div class="ticket">
+                        <p>Movie name</p>
+                        <p>12.04.2024</p>
+                        <p>16:30</p>
+                    </div>
+                    <div class="ticket">
+                        <p>Movie name</p>
+                        <p>12.04.2024</p>
+                        <p>16:30</p>
+                    </div>
+                    <div class="ticket">
+                        <p>Movie name</p>
+                        <p>12.04.2024</p>
+                        <p>16:30</p>
+                    </div>
+                    <div class="ticket">
+                        <p>Movie name</p>
+                        <p>12.04.2024</p>
+                        <p>16:30</p>
+                    </div>
+                    <div class="ticket">
+                        <p>Movie name</p>
+                        <p>12.04.2024</p>
+                        <p>16:30</p>
+                    </div>
+                    <div class="ticket">
+                        <p>Movie name</p>
+                        <p>12.04.2024</p>
+                        <p>16:30</p>
+                    </div>
+                    <div class="ticket">
+                        <p>Movie name</p>
+                        <p>12.04.2024</p>
+                        <p>16:30</p>
+                    </div>
+                    <div class="ticket">
+                        <p>Movie name</p>
+                        <p>12.04.2024</p>
+                        <p>16:30</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Profile;
