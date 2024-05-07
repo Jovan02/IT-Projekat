27c4a2f999782012fc5c7c1d5215c4e23dda3ac3
Jovan02 <jovannikolic2801@gmail.com>
2024-05-07 19:33:40 +0200

    Add Home design

---
+++ client/front/src/pages/home.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import "../styles/Home.css";
+const Home = () => {
+    return (
+        <div class="main">
+            <div class="title">
+                <h1>CINEMAXX</h1>
+            </div>
+
+            <div class="card-container">
+                <div class="card"></div>
+
+                <div class="card"></div>
+
+                <div class="card"></div>
+
+                <div class="card"></div>
+
+                <div class="card"></div>
+
+                <div class="card"></div>
+
+                <div class="card"></div>
+
+                <div class="card"></div>
+
+                <div class="card"></div>
+
+                <div class="card"></div>
+
+                <div class="card"></div>
+
+                <div class="card"></div>
+            </div>
+        </div>
+    );
+};
+export default Home;
