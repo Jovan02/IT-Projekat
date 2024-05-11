d506a0281ad39551156a08d1d911f99c3e0b0e62
Jovan02 <jovannikolic2801@gmail.com>
2024-05-11 17:47:13 +0200

    Create getReviews on backend

--- server/controllers/reviews.js
+++ server/controllers/reviews.js
@@ -1,4 +1,17 @@
-const getReviews = (req, res) => {};
+const db = require("../db");
+
+const getReviews = (req, res) => {
+    const query = `SELECT * FROM review WHERE MovieID = ?`;
+    db.query(query, [req.body.movieId], (err, result) => {
+        if (err) {
+            res.status(500).json(err);
+        } else if (result.length === 0) {
+            res.status(404).json("No reviews found");
+        } else {
+            res.status(200).json("Reviews...");
+        }
+    });
+};
 
 const createReview = (req, res) => {};
 
