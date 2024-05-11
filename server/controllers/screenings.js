cc05b8ddfccc7c4b40cac0423bf38916e4f668a1
Jovan02 <jovannikolic2801@gmail.com>
2024-05-11 17:47:23 +0200

    Create getScreenings on backend

--- server/controllers/screenings.js
+++ server/controllers/screenings.js
@@ -1,4 +1,17 @@
-const getScreenings = (req, res) => {};
+const db = require("../db");
+
+const getScreenings = (req, res) => {
+    const query = `SELECT * FROM screening WHERE MovieID = ?`;
+    db.query(query, [req.body.movieId], (err, result) => {
+        if (err) {
+            res.status(500).json(err);
+        } else if (result.length === 0) {
+            res.status(404).json("No screenings found");
+        } else {
+            res.status(200).json("Screenings...");
+        }
+    });
+};
 
 const createScreening = (req, res) => {};
 
