1b7e63642ae08bed34c43bf2912b7bf7a1f0a420
Jovan02 <jovannikolic2801@gmail.com>
2024-05-11 17:46:40 +0200

    Create login and register on backend

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,6 +1,47 @@
-const register = (req, res) => {};
+const db = require("../db");
 
-const login = (req, res) => {};
+const register = (req, res) => {
+    const { email, username, password } = req.body;
+
+    const checkQuery = `SELECT * FROM user WHERE Username = ?`;
+    db.query(checkQuery, [username], (err, result) => {
+        if (err) {
+            res.status(500).json(err);
+            return;
+        } else if (result.length > 0) {
+            res.status(409).json("User already exists");
+            return;
+        } else {
+            const registerQuery = `INSERT INTO user (Email, Username, Password) VALUES (?, ?, ?)`;
+            db.query(
+                registerQuery,
+                [email, username, password],
+                (err, result) => {
+                    if (err) {
+                        res.status(500).json(err);
+                    } else {
+                        res.status(201).json("User registered");
+                    }
+                }
+            );
+        }
+    });
+};
+
+const login = (req, res) => {
+    const { username, password } = req.body;
+
+    const query = `SELECT * FROM user WHERE Username = ? AND Password = ?`;
+    db.query(query, [username, password], (err, result) => {
+        if (err) {
+            res.status(500).json(err);
+        } else if (result.length === 0) {
+            res.status(401).json("Invalid username or password");
+        } else {
+            res.status(200).json("Login successful");
+        }
+    });
+};
 
 const logout = (req, res) => {};
 
