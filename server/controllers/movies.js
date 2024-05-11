814531bf6781aadb79c0f1dbc21b8f423bd0347a
Jovan02 <jovannikolic2801@gmail.com>
2024-05-11 17:47:00 +0200

    Create getMovie, getMovies on backend

--- server/controllers/movies.js
+++ server/controllers/movies.js
@@ -1,6 +1,30 @@
-const getMovies = (req, res) => {};
+const db = require("../db");
 
-const getMovie = (req, res) => {};
+const getMovies = (req, res) => {
+    const query = `SELECT * FROM movie`;
+    db.query(query, [], (err, result) => {
+        if (err) {
+            res.status(500).json(err);
+        } else if (result.length === 0) {
+            res.status(404).json("No movies found");
+        } else {
+            res.status(200).json("Movies...");
+        }
+    });
+};
+
+const getMovie = (req, res) => {
+    const query = `SELECT * FROM movie WHERE ID = ?`;
+    db.query(query, [req.params.id], (err, result) => {
+        if (err) {
+            res.status(500).json(err);
+        } else if (result.length === 0) {
+            res.status(404).json("Movie not found");
+        } else {
+            res.status(200).json("Movie");
+        }
+    });
+};
 
 const createMovie = (req, res) => {};
 
