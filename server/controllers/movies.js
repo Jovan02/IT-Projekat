6bbce21dca73a33df2b16170e78b779334caaeae
Jovan02 <jovannikolic2801@gmail.com>
2024-05-12 21:25:51 +0200

    Add promises to movies

--- server/controllers/movies.js
+++ server/controllers/movies.js
@@ -26,6 +26,73 @@ const getMovie = (req, res) => {
     });
 };
 
-const createMovie = (req, res) => {};
+function findMovieIdPromise() {
+    const findIdQuery = `SELECT ID FROM movie ORDER BY ID DESC LIMIT 1`;
+    return new Promise((resolve, reject) => {
+        db.query(findIdQuery, [], (err, res) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(res[0].ID);
+            }
+        });
+    });
+}
+
+function addMoviePromise(name, image, description, duration) {
+    const insertMovieQuery = `INSERT INTO movie(Name, Image, Description, Duration) VALUES(?, ?, ?, ?)`;
+    return new Promise((resolve, reject) => {
+        db.query(
+            insertMovieQuery,
+            [name, image, description, duration],
+            (err, res) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(res);
+                }
+            }
+        );
+    });
+}
+
+function addGenresPromise(movieId, genres) {
+    const insertGenresQuery = `INSERT INTO moviegenre(MovieID, GenreName) VALUES (?, ?)`;
+    return new Promise((resolve, reject) => {
+        genres.map((genre) => {
+            db.query(insertGenresQuery, [movieId, genre], (err, res) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(res);
+                }
+            });
+        });
+    });
+}
+
+const createMovie = async (req, res) => {
+    const { name, image, description, duration, genres } = req.body;
+
+    try {
+        const addMovie = await addMoviePromise(
+            name,
+            image,
+            description,
+            duration
+        );
+
+        const findMovieIdResult = await findMovieIdPromise();
+
+        const insertGenresQuery = await addGenresPromise(
+            findMovieIdResult,
+            genres
+        );
+
+        res.status(200).json("Added successfully.");
+    } catch (error) {
+        res.status(500).json(error);
+    }
+};
 
 module.exports = { getMovies, getMovie, createMovie };
