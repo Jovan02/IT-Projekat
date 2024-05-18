8bc4b7128fb7f53489d317da743d5ecc4cc860bf
Jovan02 <jovannikolic2801@gmail.com>
2024-05-18 19:43:19 +0200

    Add folder for images

--- server/server.js
+++ server/server.js
@@ -5,12 +5,16 @@ const screeningsRoutes = require("./routes/screenings");
 const reviewsRoutes = require("./routes/reviews");
 const ticketsRoutes = require("./routes/tickets");
 const usersRoutes = require("./routes/users");
+const genresRoutes = require("./routes/genres");
+const imagesRoutes = require("./routes/images");
 const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(express.static("./public"));
+app.use("/public/images", express.static(__dirname + "/public/images/"));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/movies", moviesRoutes);
@@ -18,6 +22,8 @@ app.use("/api/screenings", screeningsRoutes);
 app.use("/api/reviews", reviewsRoutes);
 app.use("/api/tickets", ticketsRoutes);
 app.use("/api/users", usersRoutes);
+app.use("/api/genres", genresRoutes);
+app.use("/api/images", imagesRoutes);
 
 app.listen(8000, () => {
     console.log("Server is running on port 8000");
