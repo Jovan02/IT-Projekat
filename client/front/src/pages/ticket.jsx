f313755131757c13bf94b1c978f6b2d704c4b96a
Jovan02 <jovannikolic2801@gmail.com>
2024-05-18 22:53:19 +0200

    Fix style bug

--- client/front/src/pages/ticket.jsx
+++ client/front/src/pages/ticket.jsx
@@ -123,143 +123,165 @@ const Ticket = () => {
             </div>
 
             <div className="main-container">
-                <h2 className="movie-title">{screeningMovie.Name}</h2>
-                <p className="date-text">
-                    {new Date(screeningMovie.Date).toDateString()}
-                </p>
-                <p className="time-text">{screeningMovie.Time}</p>
+                <div className="ticket-container">
+                    <h2 className="movie-title">{screeningMovie.Name}</h2>
+                    <p className="date-text">
+                        {new Date(screeningMovie.Date).toDateString()}
+                    </p>
+                    <p className="time-text">{screeningMovie.Time}</p>
 
-                <div className="seat-container">
-                    {Array.from({ length: 14 }).map((_, index) => {
-                        if (index == 0 || index == 6)
-                            return <div className="seat"></div>;
-                        else if (index > 6)
-                            return <div className="seat">{index - 1}</div>;
-                        else return <div className="seat">{index}</div>;
-                    })}
+                    <div className="seat-container">
+                        {Array.from({ length: 14 }).map((_, index) => {
+                            if (index == 0 || index == 6)
+                                return <div className="seat"></div>;
+                            else if (index > 6)
+                                return <div className="seat">{index - 1}</div>;
+                            else return <div className="seat">{index}</div>;
+                        })}
 
-                    {rows.map((letter, indexRow) => {
-                        if (indexRow <= 5) {
-                            return (
-                                <>
-                                    <div
-                                        className={
-                                            "seat row--" + String(indexRow + 1)
-                                        }
-                                    >
-                                        {letter}
-                                    </div>
-                                    {Array.from({ length: 13 }).map(
-                                        (_, indexCol) => {
-                                            if (indexCol == 5)
-                                                return (
-                                                    <div className="seat"></div>
-                                                );
-                                            else
-                                                return (
-                                                    <div
-                                                        className={
-                                                            "seat row--" +
-                                                            String(
-                                                                indexRow + 1
-                                                            ) +
-                                                            " col--" +
-                                                            String(indexCol + 1)
-                                                        }
-                                                    >
-                                                        <Seat
-                                                            row={indexRow + 1}
-                                                            column={
-                                                                indexCol + 1
-                                                            }
-                                                            reference={(el) =>
-                                                                seatsRef.current.push(
-                                                                    el
-                                                                )
-                                                            }
-                                                            taken={false}
-                                                            clickEvent={(e) =>
-                                                                handleSeatClick(
-                                                                    e,
-                                                                    indexRow +
-                                                                        1,
+                        {rows.map((letter, indexRow) => {
+                            if (indexRow <= 5) {
+                                return (
+                                    <>
+                                        <div
+                                            className={
+                                                "seat row--" +
+                                                String(indexRow + 1)
+                                            }
+                                        >
+                                            {letter}
+                                        </div>
+                                        {Array.from({ length: 13 }).map(
+                                            (_, indexCol) => {
+                                                if (indexCol == 5)
+                                                    return (
+                                                        <div className="seat"></div>
+                                                    );
+                                                else
+                                                    return (
+                                                        <div
+                                                            className={
+                                                                "seat row--" +
+                                                                String(
+                                                                    indexRow + 1
+                                                                ) +
+                                                                " col--" +
+                                                                String(
                                                                     indexCol + 1
                                                                 )
                                                             }
-                                                        />
-                                                    </div>
-                                                );
-                                        }
-                                    )}
-                                </>
-                            );
-                        } else {
-                            return (
-                                <>
-                                    <div
-                                        className={
-                                            "seat row--" + String(indexRow + 1)
-                                        }
-                                    >
-                                        {letter}
-                                    </div>
-                                    {Array.from({ length: 13 }).map(
-                                        (_, indexCol) => {
-                                            if (indexCol == 5)
-                                                return (
-                                                    <div className="seat"></div>
-                                                );
-                                            else if (indexCol >= 10)
-                                                return (
-                                                    <div className="seat"></div>
-                                                );
-                                            else
-                                                return (
-                                                    <div
-                                                        className={
-                                                            "seat row--" +
-                                                            String(
-                                                                indexRow + 1
-                                                            ) +
-                                                            " col--" +
-                                                            String(indexCol + 1)
-                                                        }
-                                                    >
-                                                        <Seat
-                                                            row={indexRow + 1}
-                                                            column={
-                                                                indexCol + 1
-                                                            }
-                                                            reference={(el) =>
-                                                                seatsRef.current.push(
+                                                        >
+                                                            <Seat
+                                                                row={
+                                                                    indexRow + 1
+                                                                }
+                                                                column={
+                                                                    indexCol + 1
+                                                                }
+                                                                reference={(
                                                                     el
-                                                                )
-                                                            }
-                                                            taken={false}
-                                                            clickEvent={(e) =>
-                                                                handleSeatClick(
-                                                                    e,
-                                                                    indexRow +
-                                                                        1,
+                                                                ) =>
+                                                                    seatsRef.current.push(
+                                                                        el
+                                                                    )
+                                                                }
+                                                                taken={false}
+                                                                clickEvent={(
+                                                                    e
+                                                                ) =>
+                                                                    handleSeatClick(
+                                                                        e,
+                                                                        indexRow +
+                                                                            1,
+                                                                        indexCol +
+                                                                            1
+                                                                    )
+                                                                }
+                                                            />
+                                                        </div>
+                                                    );
+                                            }
+                                        )}
+                                    </>
+                                );
+                            } else {
+                                return (
+                                    <>
+                                        <div
+                                            className={
+                                                "seat row--" +
+                                                String(indexRow + 1)
+                                            }
+                                        >
+                                            {letter}
+                                        </div>
+                                        {Array.from({ length: 13 }).map(
+                                            (_, indexCol) => {
+                                                if (indexCol == 5)
+                                                    return (
+                                                        <div className="seat"></div>
+                                                    );
+                                                else if (indexCol >= 10)
+                                                    return (
+                                                        <div className="seat"></div>
+                                                    );
+                                                else
+                                                    return (
+                                                        <div
+                                                            className={
+                                                                "seat row--" +
+                                                                String(
+                                                                    indexRow + 1
+                                                                ) +
+                                                                " col--" +
+                                                                String(
                                                                     indexCol + 1
                                                                 )
                                                             }
-                                                        />
-                                                    </div>
-                                                );
-                                        }
-                                    )}
-                                </>
-                            );
-                        }
-                    })}
+                                                        >
+                                                            <Seat
+                                                                row={
+                                                                    indexRow + 1
+                                                                }
+                                                                column={
+                                                                    indexCol + 1
+                                                                }
+                                                                reference={(
+                                                                    el
+                                                                ) =>
+                                                                    seatsRef.current.push(
+                                                                        el
+                                                                    )
+                                                                }
+                                                                taken={false}
+                                                                clickEvent={(
+                                                                    e
+                                                                ) =>
+                                                                    handleSeatClick(
+                                                                        e,
+                                                                        indexRow +
+                                                                            1,
+                                                                        indexCol +
+                                                                            1
+                                                                    )
+                                                                }
+                                                            />
+                                                        </div>
+                                                    );
+                                            }
+                                        )}
+                                    </>
+                                );
+                            }
+                        })}
+                    </div>
+
+                    <div className="screen"></div>
+
+                    <button className="button-buy" onClick={handleBuyTicket}>
+                        Buy Ticket
+                    </button>
                 </div>
-
-                <div className="screen"></div>
-
-                <button className="button-buy" onClick={handleBuyTicket}>
-                    Buy Ticket
-                </button>
             </div>
         </div>
     );
