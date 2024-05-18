dfa7cbf4d41832404f7c974c5e2fbfca79daf4b9
Jovan02 <jovannikolic2801@gmail.com>
2024-05-18 19:46:11 +0200

    Add functionality to add movie

--- client/front/src/pages/AdminPanel.jsx
+++ client/front/src/pages/AdminPanel.jsx
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "../styles/AdminPanel.css";
 import axios from "axios";
 import { useState } from "react";
 
 const AdminPanel = () => {
     const URL = "http://localhost:8000";
+
+    const [users, setUsers] = useState([]);
+    const [genres, setGenres] = useState([]);
+    const [file, setFile] = useState(null);
+    const [imgFile, setImgFile] = useState("");
+    const [movieName, setMovieName] = useState("");
+    const [movieDescription, setMovieDescription] = useState("");
+    const [movieDuration, setMovieDuration] = useState("");
+    const [movieGenres, setMovieGenres] = useState([]);
 
     const handleChooseTab = (e) => {
         const buttons = document.querySelectorAll(".button");
@@ -32,13 +41,20 @@ const AdminPanel = () => {
         }
     };
 
-    const [users, setUsers] = useState([]);
-
     const loadUsers = async () => {
         try {
             const response = await axios.get(`${URL}/api/users`);
             setUsers(response.data);
             console.log(response.data);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    const loadGenres = async () => {
+        try {
+            const response = await axios.get(`${URL}/api/genres`);
+            setGenres(response.data);
         } catch (error) {
             console.log(error);
         }
@@ -54,6 +70,71 @@ const AdminPanel = () => {
         }
     };
 
+    const handleFileChange = (e) => {
+        setFile(e.target.files[0]);
+        console.log(e.target.files[0]);
+    };
+
+    const handleAddImage = async () => {
+        const data = new FormData();
+        data.append("file", file);
+        try {
+            const response = await axios.post(`${URL}/api/images`, data);
+            console.log(response.data.filename);
+            setImgFile(`${URL}/public/images/${response.data.filename}`);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    const handleAddMovie = async () => {
+        try {
+            await handleAddImage();
+            const movieData = {
+                name: movieName,
+                image: imgFile,
+                description: movieDescription,
+                duration: movieDuration,
+                genres: movieGenres,
+            };
+            const response = await axios.post(`${URL}/api/movies`, movieData);
+            console.log(response);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    const handleTitleChange = (e) => {
+        setMovieName(e.target.value);
+    };
+
+    const handleDurationChange = (e) => {
+        setMovieDuration(e.target.value);
+    };
+
+    const handleDescriptionChange = (e) => {
+        setMovieDescription(e.target.value);
+    };
+
+    const handleGenreCheckbox = (e) => {
+        if (e.target.checked) {
+            setMovieGenres([...movieGenres, e.target.name]);
+        } else {
+            setMovieGenres(
+                movieGenres.filter((genre) => genre !== e.target.name)
+            );
+        }
+    };
+
+    useEffect(() => {
+        loadUsers();
+        loadGenres();
+    }, []);
+
+    useEffect(() => {
+        console.log(movieGenres);
+    }, [movieGenres]);
+
     return (
         <div class="main">
             <div class="title">
@@ -61,6 +142,7 @@ const AdminPanel = () => {
             </div>
 
             <div class="main-container">
+                <img src={imgFile} alt="logo" class="logo" />
                 <div class="choice-buttons">
                     <button class="button" onClick={handleChooseTab}>
                         Manage Users
@@ -92,28 +174,58 @@ const AdminPanel = () => {
                 <div class="container add-movie-container">
                     <form>
                         <label for="title">Title</label>
-                        <input type="text" id="title" name="title" required />
+                        <input
+                            type="text"
+                            id="title"
+                            name="title"
+                            required
+                            onChange={handleTitleChange}
+                        />
                         <label for="image">Image</label>
-                        <input type="file" id="image" name="image" required />
+                        <input
+                            type="file"
+                            id="image"
+                            name="image"
+                            required
+                            multiple={false}
+                            onChange={handleFileChange}
+                        />
                         <label for="genre">Genre</label>
-                        <input type="text" id="genre" name="genre" required />
+                        <div class="genre-checkbox-container">
+                            {genres.map((genre) => (
+                                <label class="checkbox-box">
+                                    {genre.Name}
+                                    <input
+                                        type="checkbox"
+                                        name={genre.Name}
+                                        onChange={handleGenreCheckbox}
+                                    />
+                                    <span class="checkmark"></span>
+                                </label>
+                            ))}
+                        </div>
                         <label for="duration">Duration</label>
                         <input
                             type="text"
                             id="duration"
                             name="duration"
                             required
+                            onChange={handleDurationChange}
                         />
                         <label for="description">Description</label>
-                        <div
+                        <textarea
                             class="description"
                             id="description"
                             name="description"
                             contenteditable=""
-                        ></div>
+                            required
+                            onChange={handleDescriptionChange}
+                        ></textarea>
                     </form>
 
-                    <button class="button-movie">Add</button>
+                    <button class="button-movie" onClick={handleAddMovie}>
+                        Add
+                    </button>
                 </div>
 
                 <div class="container add-screening">
